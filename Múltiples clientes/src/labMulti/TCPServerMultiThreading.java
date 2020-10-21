e48a7927c1c76691502136ca973aeab555f8775d
Joel Sola <joel.sola@estudiants.urv.cat>
2020-10-21 15:35:54 +0200

    Base con ficheros moodle

---
+++ Múltiples clientes/src/labMulti/TCPServerMultiThreading.java
@@ -0,0 +1,16 @@
+package labMulti;
+
+import java.net.*;
+
+class TCPServerMultiThreading {
+	public static void main(String argv[]) throws Exception {
+		
+		
+		ServerSocket welcomeSocket = new ServerSocket(1234);
+
+		while (true) {
+			Socket connectionSocket = welcomeSocket.accept();
+			new AttendPetition(connectionSocket).start();
+		}
+	}
+}
